a2564adb0574bea35f3723c6b13988892adf7e73
Mark Meves <mark.meves@gmail.com>
2010-11-04 00:15:36 -0400

    find modules in a variety of ways. about to refactor

--- hipe-code-molester/lib/hipe-code-molester.rb
+++ hipe-code-molester/lib/hipe-code-molester.rb
@@ -1,6 +1,8 @@
 # require 'filetuils'
 require 'ruby_parser'
 require 'ruby2ruby'
+require 'pp'
+require 'strscan'
 
 module Hipe; end
 class Hipe::CodeMolester
@@ -22,10 +24,15 @@ class Hipe::CodeMolester
 end
 
 class Hipe::CodeMolester
+  def initialize
+    @sexp = nil        # yeah
+  end
+  attr_reader :sexp
   def file path, &block
     @sexp = nil
     @path = path
-    @fh = File.open(path)
+    str = File.read(path)
+    self.ruby = str
     yield self if block_given?
     self
   end
@@ -37,5 +44,71 @@ class Hipe::CodeMolester
     end
     yield self if block_given?
     self
+  end
+  def ruby *a
+    case a.size
+    when 1 ; self.ruby= a.first; return self;
+    when 0 ; # fallthrough
+    else raise ArgumentError.new("0 or 1 argument")
+    end
+    Ruby2RubyMolested.new.process @sexp.deep_clone # sexp_processor
+  end
+  def ruby= str
+    sexp = RubyParser.new.parse(str) or fail("huh?")
+    @sexp = sexp
+    str
+  end
+  def module str
+    is_toplevel, const, rest = parse_module_path str
+    @sexp.nil? and return nil
+    case @sexp.first
+    when :block
+      modules =  @sexp.select{ |node| Sexp === node && node.first == :module }
+      matches = modules.select{ |node| node[1] == const }
+      case matches.size
+      when 0 ; nil
+      when 1 ;
+        case modules.size
+        when 1 ; rest ? self.module(rest) : self
+        else   ;
+          next_node = self.class.cached(matches.first)
+          rest ? next_node.module(rest) : next_node
+        end
+      else
+        children = matches.map{ |node| self.class.cached(node) }
+        rest.nil? ? children : begin
+          things = children.map{ |child| child.module(rest) }.compact.flatten
+          things.empty? ? nil : things
+        end
+      end
+    when :module
+      if @sexp[1] != const
+        nil
+      else
+        rest.nil? ? self : nil
+      end
+    else nil
+    end
+  end
+private
+  def parse_module_path str
+    md = /\A(::)?([a-z0-9_]+(?:::[a-z0-9_]+)*)\z/i.match(str) or fail("bad module path: #{str.inspect}")
+    scn = StringScanner.new(md[2])
+    const = scn.scan(/[^:]+/).intern
+    scn.scan(/::/)
+    rest = ( '' == scn.rest ? nil : scn.rest )
+    [md[0], const, rest]
+  end
+  @@cache = {}
+  class << self
+    def cached sexp
+      @@cache[sexp.object_id] ||= new_child(sexp)
+      @@cache[sexp.object_id]
+    end
+    def new_child sexp
+      child = allocate
+      child.instance_variable_set('@sexp', sexp)
+      child
+    end
   end
 end
--- hipe-code-molester/test/test-the-molester.rb
+++ hipe-code-molester/test/test-the-molester.rb
@@ -6,27 +6,106 @@ CodeMolester = Hipe::CodeMolester
 
 class TestMolester < MiniTest::Unit::TestCase
   @@curr = 0
+  @@verbose = nil
   def next_empty_tmpdir
     dir = "#{Dir.tmpdir}/molest-#{@@curr += 1}"
-    File.directory?(dir) || FileUtils.mkdir(dir, :verbose => 1)
-    Dir["#{dir}/*"].empty? || FileUtils.rm_rf("#{dir}/*", :verbose => 1)
+    File.directory?(dir) || FileUtils.mkdir(dir, :verbose => @@verbose)
+    unless Dir["#{dir}/*"].empty?
+      Dir["#{dir}/*"].each do |path|
+        @verbose and $stdout.puts("rm -rf #{path}")
+        FileUtils.remove_entry_secure(path)
+      end
+    end
     dir
   end
   def setup
     @cm = CodeMolester.new
   end
-  def test_fail_when_file_not_exist
+  def _test_fail_when_file_not_exist
     dir = next_empty_tmpdir
     e = assert_raises Errno::ENOENT do
       @cm.file("#{dir}/not-exist")
     end
-    assert_match /No such file or directory/, e.message
+    assert_match(/No such file or directory/, e.message)
   end
-  def test_create_file_when_not_exist
+  def _test_create_file_when_not_exist
     dir = next_empty_tmpdir
     path = "#{dir}/make-this-file"
-    refute File.exist?(path)
+    refute File.exist?(path), 'path should not exist'
     @cm.file!(path)
-    assert File.exist?(path)
+    assert File.exist?(path), 'path should exist'
+  end
+  def test_search_module_in_empty_block_find_none
+    assert_equal nil, @cm.module('Foo')
+  end
+  def test_search_deep_module_in_emtpy_block_find_none
+    assert_equal nil, @cm.module('Foo::Bar')
+  end
+  def test_search_module_abs_path_in_empty_block_find_none
+    assert_equal nil, @cm.module('::Foo')
+  end
+  def test_search_abs_module_deep_in_empty_find_none
+    assert_equal nil, @cm.module('::Foo::Bar')
+  end
+  def test_find_one_module_should_be_self!
+    @cm.ruby('module Foo; end')
+    found = @cm.module('Foo')
+    assert_equal @cm.class, found.class
+    assert_equal @cm.object_id, found.object_id
+  end
+  def test_find_one_module_absolute
+    @cm.ruby('module Foo; end')
+    found = @cm.module('::Foo')
+    assert_equal @cm.object_id, found.object_id
+  end
+  def test_find_one_wrong_module_should_be_nil
+    @cm.ruby('module Bar; end')
+    assert_equal(nil, @cm.module('Foo'))
+  end
+  def test_find_two_right_among_two
+    @cm.ruby(<<-RUBY)
+      module Foo; end
+      module Foo; end
+    RUBY
+    founds = @cm.module('Foo') # experimental
+    assert_kind_of Array, founds
+    assert_equal founds.size, 2
+    founds2 = @cm.module('Foo')
+    assert_equal founds[0].object_id, founds2[0].object_id
+    assert_equal founds[1].object_id, founds2[1].object_id
+  end
+  def test_find_one_among_several
+    @cm.ruby(<<-RUBY)
+      module Alpha; end
+      module Beta;  end
+      module Gamma; end
+    RUBY
+    found = @cm.module('Beta')
+    assert_kind_of Hipe::CodeMolester, found
+    assert found.object_id != @cm.object_id, "should not return self for this"
+  end
+  def test_find_two_among_several
+    @cm.ruby(<<-RUBY)
+      module Alpha; end
+      module Beta;  end
+      module Gamma; end
+      module Beta;  end
+    RUBY
+    found = @cm.module('Beta')
+    assert_kind_of Array, found
+    assert_equal 2, found.size
+    assert_kind_of Hipe::CodeMolester, found.first
+    assert_kind_of Hipe::CodeMolester, found.last
+    assert(@cm.object_id != found.first.object_id, "different objects here")
+    assert(found.first.object_id != found.last.object_id, "different objects here")
+  end
+  def _test_find_one_that_is_squashed_with_two
+    @cm.ruby(<<-RUBY)
+      module Faz; end
+      module Foo::Bar::Baz; end
+      module Fipple; end
+    RUBY
+    found = @cm.module('Foo')
+    PP.pp found
   end
 end
