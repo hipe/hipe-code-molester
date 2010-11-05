553a41b83bcee2208dae02069c1400118c69be8f
Mark Meves <mark.meves@gmail.com>
2010-11-04 22:43:37 -0400

    coverage: 84.18%. (add 9%). cover some comment related stuffs

--- hipe-code-molester/test/test-file-io.rb
+++ hipe-code-molester/test/test-file-io.rb
@@ -19,4 +19,16 @@ class TestFileMolestation < MiniTest::Unit::TestCase
     @cm.file!(path)
     assert File.exist?(path), 'path should exist'
   end
+  def test_comments_zoo
+    orig_file = File.dirname(__FILE__) + '/fixtures/example-1.rb'
+    orig_contents = File.read(orig_file)
+    @cm.file(orig_file)
+    unparsed_contents = @cm.ruby
+    have = unparsed_contents.scan(/^ *# (\d+)\)/).map{ |x| x.first }
+    need = %w(1 2 3 4 5 6 8 9) # not seven or ten
+    missing = need - have
+    extra   = have - need
+    assert_equal 0, missing.size, "these comment lines should have been parsed "
+    assert_equal 0, extra.size, "these comments lines should not have been parsed"
+  end
 end
